dcf0432738559263940e0b1661cc048937d3d1bf
laura <laurafprocopio@gmail.com>
2025-01-31 12:42:34 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+<main class="columns is-gapless is-multiline" :class="{'modo-escuro' : modoEscuroAtivo }">
+  <div class="colums is-one-quarter" >
+    <BarraLateral @aoTemaAlterado="trocarTema" />
+  </div>
+  <div class="column is-three-quarter conteudo" >
+    <Notificacoes/>
+   <RouterView></RouterView>
+    </div>
+</main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import ITarefa from '../src/interfaces/ITarefa'
+import Notificacoes from './components/Notificacoes.vue';
+
+
+export default defineComponent({
+  name: 'App',
+ components: {
+    BarraLateral,
+    Notificacoes
+    
+ },
+ data () {
+  return {
+    modoEscuroAtivo: false
+  }
+ },
+ methods: {
+ trocarTema(modoEscuroAtivo: boolean){
+    this.modoEscuroAtivo = modoEscuroAtivo
+  }
+ }
+});
+</script>
+
+<style>
+.lista{
+  padding: 1.25rem ;
+}
+
+main{
+  --bg-primario: #fff;
+  --texto-primario: #000;
+}
+
+main.modo-escuro{
+  --bg-primario: #2b2d42;
+  --texto-primario: #ddd;
+}
+
+.conteudo{
+  background-color: var(--bg-primario);
+}
+</style>
